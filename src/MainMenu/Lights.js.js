dd940a0a853513c3f5f29d86b3588e522cbe96ca
Rajeev-Kapoor <rkapoor0901@gmail.com>
2024-01-17 19:03:02 +0530

    changes

--- src/MainMenu/Lights.js.js
+++ src/MainMenu/Lights.js.js
@@ -1,36 +1,37 @@
-// import mouse from  '../mouse.mp3'
-// import { useState } from 'react';
-// import bulb from '../images/bulb.gif'
-// import lightoff from'../images/lightOff.png'
-// import cielingfanoff from '../images/cielingfanoff.jpeg'
-// import ceilingfan from '../images/ceilingfan.gif'
-// import writeUserData from '../firebase/firebase';
-// import 'react-h5-audio-player/lib/styles.css';
+import mouse from  '../mouse.mp3'
+import { useState } from 'react';
+import bulb from '../images/bulb.gif'
+import lightoff from'../images/lightOff.png'
+import cielingfanoff from '../images/cielingfanoff.jpeg'
+import ceilingfan from '../images/ceilingfan.gif'
+import { updatedata } from '../firebase/firebase';
+import writeUserData from '../firebase/firebase';
+import 'react-h5-audio-player/lib/styles.css';
 import React from 'react'
 
 const Mainmenu = () => {
-  //   const[statuslight,setStatuslight]=useState()
-  //   const[switc,setSwitc]=useState("ON")
-  //   const[statusfan,setStatusfan]=useState()
+    const[statuslight,setStatuslight]=useState()
+    const[switc,setSwitc]=useState("ON")
+    const[statusfan,setStatusfan]=useState()
   
-  //   const handleStatusbulb =()=>{
-  //     setStatuslight(!statuslight)
-  //    setSwitc(statuslight?"ON":"OFF")
-  //   new Audio(mouse).play()   
+    const handleStatusbulb =()=>{
+      setStatuslight(!statuslight)
+     setSwitc(statuslight?"ON":"OFF")
+    new Audio(mouse).play()   
     
-  // writeUserData("Products","fan",switc)
+  updatedata("/Room1",'led_status '  ,switc,)
   
-  // }
+  }
     
-  //   const handleStatusfan =()=>{
-  //     setStatusfan(!statusfan)
-  //   new Audio(mouse).play()   
-  //   }
+    const handleStatusfan =()=>{
+      setStatusfan(!statusfan)
+    new Audio(mouse).play()   
+    }
     
     return (
         <div >
 <h3 style={{}}>Room Light </h3>
-{/* 
+
 {statuslight&& <img style={{height:"50px" }} src={bulb} alt ="bulb"/>}
       {!statuslight&&<img style={{height:"50px" }} src={lightoff} alt= "lightoff"/>}
 <button className ="button1" type="button" style={{color:"white", 
@@ -44,7 +45,7 @@ const Mainmenu = () => {
 <button className ="button1" type="button" style={{color:"white", 
   backgroundColor:statusfan?"red":"#04AA6D",
    textDecoration:"bold",fontSize:"large"}} onClick={handleStatusfan}>
- {statusfan?"OFF":"ON"}</button> */}
+ {statusfan?"OFF":"ON"}</button>
 
          </div>
   );
